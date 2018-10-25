43583e8bd8f593712a19a568c7a24b8ff2f8e03f
Sandeep Patil <mail2spatil@gmail.com>
2018-10-25 21:03:54 +0530

    EKF Implemented

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -25,6 +25,10 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  //Get Transpose of F
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +36,27 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd _z;
+  VectorXd y;
+  MatrixXd Ht;
+  MatrixXd S;
+  MatrixXd Si;
+  MatrixXd PHt;
+  MatrixXd K;
+
+  _z = H_ * x_;
+  y = z - _z;
+  Ht = H_.transpose();
+  S = H_ * P_ * Ht + R_;
+  Si = S.inverse();
+  PHt = P_ * Ht;
+  K = PHt * Si;
+
+  //Get new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +64,34 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  float rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+  float phi = atan2(x_(1), x_(0));
+  float rho_dot;
+  if (fabs(rho) < 0.0001) {
+    rho_dot = 0;
+  } else {
+    rho_dot = (x_(0)*x_(2) + x_(1)*x_(3))/rho;
+  }
+  
+  VectorXd z_pred(3);
+  VectorXd y;
+  MatrixXd Ht;
+  MatrixXd S;
+  MatrixXd Si;
+  MatrixXd PHt;
+  MatrixXd K;
+  
+  z_pred << rho, phi, rho_dot;
+  y = z - z_pred;
+  Ht = H_.transpose();
+  S = H_ * P_ * Ht + R_;
+  Si = S.inverse();
+  PHt = P_ * Ht;
+  K = PHt * Si;
+  
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -15,6 +15,23 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+  
+  //accumulate squared residuals
+  for (unsigned int i = 0; i < estimations.size(); ++i){
+
+      VectorXd residual = estimations[i] - ground_truth[i];
+
+      //coefficient-wise multiplication
+      residual = residual.array()*residual.array();
+      rmse += residual;
+  }
+
+  rmse = rmse / estimations.size();
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +39,28 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+  MatrixXd Hj(3, 4);
+
+  float c1 = px*px + py*py;
+  float c2 = sqrt(c1);
+  float c3 = (c1*c2);
+
+  //check for division by zero
+  if (fabs(c1) < 0.0001)
+  {
+      std::cout << "Error: Division by Zero" << std::endl;
+      return Hj;
+  }
+
+  //compute the Jacobian matrix
+  Hj << (px / c2), (py / c2), 0, 0,
+      -(py / c1), (px / c1), 0, 0,
+      py*(vx*py - vy*px) / c3, px*(px*vy - py*vx) / c3, px / c2, py / c2;
+
+  return Hj;
 }
